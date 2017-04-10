4952b615cf7ce252cca27007ee6288fea96067ac
philippG777 <philipp.gretzl@smaxtec.com>
2017-04-10 12:12:31 +0200

    Device has now a class

--- SEMClient/__init__.py
+++ SEMClient/__init__.py
@@ -4,13 +4,27 @@ import requests
 
 __version__ = "0.1"
 
+
+class Device(object):
+    def __init__(self, raw):
+        self.raw = raw
+
+
+    def get(self, field):
+        return self.raw[field]
+
+
+    def __repr__(self):
+        return "ID: %d | Name: %s | Location: %s" % (self.get("deviceId"), self.get("name"), self.get("room"))
+
+
 class Client(object):
     apiVersion = "vX"   # test-version??
     def __init__(self, ip):
         self.ip = ip
         self.apiAddress = "http://" + ip + "/api/" + self.apiVersion
     
-    
+
     def getDeviceIds(self):
         req = requests.get(self.apiAddress + "/device")
         return (req.json())["deviceIds"]
@@ -18,14 +32,31 @@ class Client(object):
 
     def getDevice(self, id):
         req = requests.get(self.apiAddress + "/device/" + str(id))
-        return req.json()
+        return Device(req.json())
 
 
     def getDevices(self):
         deviceIds = self.getDeviceIds()
-        devices = {}
+        devices = []
         for id in deviceIds:
-            device = self.getDevice(id)
-            devices[id] = device
-            
+            devices.append(self.getDevice(id))
         return devices
+    
+
+    def getDeviceByName(self, name):
+        devices = self.getDevices()
+        for dev in devices:
+            if dev.get("name") == name:
+                return dev
+
+    def getStatusFields(self):
+        req = requests.get(self.apiAddress + "/status/")
+        return (req.json())["availableDataNames"]
+
+
+    def getStatus(self, field):
+        req = requests.get(self.apiAddress + "/status/" + field)
+        return req.json()
+
+    def getConsumption(self):
+        return float((self.getStatus("consumption"))["valueNumeric"])
--- test.py
+++ test.py
@@ -2,5 +2,23 @@ import SEMClient
 
 sem = SEMClient.Client("192.168.168.33")
 
+print "device IDs"
 print sem.getDeviceIds()
+
+print "\ndevices"
 print sem.getDevices()
+
+print "\nbyName"
+print sem.getDeviceByName("Smart Plug")
+
+print "\nstatusFields"
+print sem.getStatusFields()
+
+print "\nstatusField:consumption"
+print sem.getStatus("consumption")
+
+print "\nConsumption"
+print sem.getConsumption()
+
+print "\ndevice with id = 1"
+print sem.getDevice(1)
